06240562743e60cdb1c58c6327c68f4f5ae806ae
Ryan Brooks <ryan@slatehorse.com>
2018-08-22 15:21:55 +0100

    Acceptance criteria extracted from the requirements PDF

--- spec/exchange_rate_spec.rb
+++ spec/exchange_rate_spec.rb
@@ -1,9 +1,37 @@
+# These are the high-level acceptance tests
+# for ExchangeRate
 RSpec.describe ExchangeRate do
   it "has a version number" do
     expect(ExchangeRate::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "#at" do
+    xit 'uses the local cache of the feed values' do
+    end
+
+    xit 'raises ExchangeRate::NoData when the requested value does not exist locally' do
+    end
   end
+
+  describe "#retrieve" do
+    xit 'fetches the feed and stores the values locally' do
+    end
+
+    xit 'overwrites existing values' do
+    end
+
+    xit 'raises ExchangeRate::RetrievalFailed when the the feed cannot be retrieved' do
+    end
+  end
+
+  describe "configure" do
+    xit 'can use a custom database connection' do
+    end
+
+    xit 'can use a custom FX rate provider' do
+    end
+  end
+
+
+  
 end
