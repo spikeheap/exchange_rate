4feb1de4d7c56cbd222b64be6b5e769d6f39f839
Ryan Brooks <ryan@slatehorse.com>
2018-08-23 14:37:22 +0100

    Adds a WIP database connection using ActiveRecord, untested

---
+++ lib/exchange_rate/database_connection.rb
@@ -0,0 +1,30 @@
+require 'yaml'
+
+module ExchangeRate
+  class DatabaseConnection
+    # TODO test
+    # FIXME: this won't play well with another AR instance
+    def self.connect
+      db_settings = YAML.load_file('db/database.yml')
+      ActiveRecord::Base.establish_connection(db_settings[environment])
+
+      self.apply_schema
+    end
+
+    def self.apply_schema
+      # FIXME: this won't stand up to much, it recreates each time
+      # FIXME: but if we don't it tries to create and fails
+      ActiveRecord::Schema.define do
+        create_table(:currency_rates, force: true, timestamps: true) do |t|
+          t.string  :currency, null: false
+          t.numeric :value_in_euro, null: false
+          t.date    :date_of_rate, null: false
+        end
+      end
+    end
+
+    def self.environment
+      ENV['environment'] || 'development'
+    end
+  end
+end
---
+++ spec/exchange_rate/database_connection_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe ExchangeRate::DatabaseConnection do
+  describe '#connect' do
+    #TODO
+  end
+
+  describe '#apply_schema' do
+    #TODO
+  end
+
+  describe '#environment' do
+    #TODO
+  end
+end
