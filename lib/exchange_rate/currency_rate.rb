b86e9daf710178f9d17761f6800942126c3a853a
Ryan Brooks <ryan@slatehorse.com>
2018-08-23 11:27:17 +0100

    Adds CurrencyRate ActiveRecord class (WIP, not backed by live store yet)

---
+++ lib/exchange_rate/currency_rate.rb
@@ -0,0 +1,9 @@
+require 'active_record'
+
+module ExchangeRate
+  class CurrencyRate < ActiveRecord::Base
+    validates :currency, presence: true
+    validates :value_in_euro, presence: true
+    validates :date_of_rate, presence: true
+  end
+end
