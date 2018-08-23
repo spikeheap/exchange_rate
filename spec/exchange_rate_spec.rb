a154b6684e3db8413b406380db6dfb9aa86423d8
Ryan Brooks <ryan@slatehorse.com>
2018-08-23 10:11:59 +0100

    Add acceptance test for retrieve and at public methods

--- spec/exchange_rate_spec.rb
+++ spec/exchange_rate_spec.rb
@@ -1,12 +1,19 @@
+require 'date'
+
 # These are the high-level acceptance tests
 # for ExchangeRate
 RSpec.describe ExchangeRate do
+  # This date references a value in the VCR for the tests, and will
+  # likely need updating when the VCR is re-recorded
+  let(:fx_rate_date) { Date.parse('2018-08-22') }
+
   it "has a version number" do
     expect(ExchangeRate::VERSION).not_to be nil
   end
 
   describe "#at" do
-    xit 'uses the local cache of the feed values' do
+    it 'uses the local cache of the feed values' do
+      expect(ExchangeRate.at(fx_rate_date,'GBP','USD')).to eq(1.291700027)
     end
 
     xit 'raises ExchangeRate::NoData when the requested value does not exist locally' do
@@ -14,12 +21,23 @@ RSpec.describe ExchangeRate do
   end
 
   describe "#retrieve" do
-    xit 'fetches the feed and stores the values locally' do
+
+    it 'fetches the feed and stores the values locally' do
+      ExchangeRate.retrieve
+
+      # Check the right number of records were saved (32 currencies * 90 days)
+      expect(ExchangeRate::CurrencyRate.count).to eq(2880)
+
+      # Spot check a couple of values
+      expect(ExchangeRate::CurrencyRate.where(exchange_date: fx_rate_date, currency: 'USD')).to eq(1.1616)
+      expect(ExchangeRate::CurrencyRate.where(exchange_date: fx_rate_date, currency: 'GBP')).to eq(0.89928)
     end
 
     xit 'overwrites existing values' do
     end
 
+    xit 'raises ExchangeRate::RetrievalFailed when the the feed cannot be retrieved' do
+    end
     xit 'raises ExchangeRate::RetrievalFailed when the the feed cannot be retrieved' do
     end
   end
